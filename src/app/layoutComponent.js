bfb0b8b24ff31a6573fa4eec84c1af895b71bfe0
amiribnjaman <amiribnjaman@gmail.com>
2024-04-01 16:49:35 +0600

    Sidebar implemented

--- src/app/layoutComponent.js
+++ src/app/layoutComponent.js
@@ -2,25 +2,26 @@
 
 import TopNavbar from "@/components/topNavbar";
 import { useEffect, useState } from "react";
+import Sidebar from "@/components/sidebar";
 
 export default function LayoutComponent({ children }) {
   // Those are declare here to props drillings (awful)
 
-
   return (
     <div className="md:flex ">
-
-      <div className="bg-[#F8F7FA] md:w-[100%] h-full">
+      <aside className="md:block hidden w-64 h-full shadow-md">
+        <Sidebar />
+      </aside>
+      <div className="bg-[#F8F7FA] md:w-[80%] h-full">
         <div className="px-6 relative">
           {/* Top Navbar  */}
-          <TopNavbar
-          />
-            <div className="min-h-[85vh] md:flex gap-6 justify-between">
-              <div className="mt-4 p-4 pb-6 md:w-[65%] border">
-                {/* <ToastContainer position="top-center" /> */}
-                {children}
-              </div>
+          <TopNavbar />
+          <div className="min-h-[85vh] md:flex gap-6 justify-between">
+            <div className="mt-4 p-4 pb-6 md:w-[65%] border">
+              {/* <ToastContainer position="top-center" /> */}
+              {children}
             </div>
+          </div>
         </div>
       </div>
     </div>
